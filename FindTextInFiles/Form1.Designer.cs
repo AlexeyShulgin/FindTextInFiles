a43b9b0e522e96610dc3b4f714f3de627073dadd
AlexeyShulgin <shulgin97@i.ua>
2016-05-23 09:26:45 +0300

    FindTextInFiles
    Application for the search the same text in files by (C#, Windows Forms)

---
+++ FindTextInFiles/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(101, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить файлы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Текст, который надо найти:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(101, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Поиск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 142);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(330, 190);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Текст";
+            this.columnHeader1.Width = 131;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Файл";
+            this.columnHeader2.Width = 145;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Строка";
+            this.columnHeader3.Width = 49;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox1.Image = global::FindTextInFiles.Properties.Resources.loader;
+            this.pictureBox1.Location = new System.Drawing.Point(257, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(51, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Всего файлов: 0";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button3.Location = new System.Drawing.Point(257, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(354, 344);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Поиск текста в файлах";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+
+    }
+}
+
---
+++ FindTextInFiles/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        List<string> FileList = new List<string>();
+
+        Thread hThread = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Все файлы|*.*";
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach(string filename in ofd.FileNames)
+                {
+                    if(!FileList.Contains(filename))
+                        FileList.Add(filename);
+                }
+            }
+            label2.Text = String.Format("Всего файлов: {0}", FileList.Count);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (FileList.Count == 0)
+            {
+                MessageBox.Show("Файлы не выбраны", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (String.IsNullOrEmpty(textBox1.Text))
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                textBox1.Enabled = false;
+                listView1.Items.Clear();
+                hThread = new Thread(Compare);
+                hThread.Start();
+            }
+            else
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                textBox1.Enabled = false;
+                listView1.Items.Clear();
+                hThread = new Thread(Find);
+                hThread.Start();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if(hThread != null)
+                if (hThread.IsAlive)
+                    hThread.Abort();
+        }
+
+        private void Find()
+        {
+            try
+            {
+                pictureBox1.Invoke(new ThreadStart(delegate
+                {
+                    pictureBox1.Visible = true;
+                }));
+
+                foreach(string FileName in FileList)
+                {
+                    int cnt = 0;
+                    StreamReader sr = new StreamReader(FileName, Encoding.Default);
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        cnt++;
+                        if (line.IndexOf(textBox1.Text,StringComparison.InvariantCultureIgnoreCase) >= 0)
+                        {
+                            if (this.InvokeRequired)
+                                listView1.Invoke(new ThreadStart(delegate
+                                {
+                                    listView1.Items.Add(new ListViewItem(new string[] { line, FileName, cnt.ToString() }));
+                                }));
+                            else
+                                listView1.Items.Add(new ListViewItem(new string[] { line, FileName, cnt.ToString() }));
+                        }
+                    }
+                    sr.Close();
+                }
+                pictureBox1.Invoke(new ThreadStart(delegate
+                {
+                    pictureBox1.Visible = false;
+                }));
+                textBox1.Invoke(new ThreadStart(delegate
+                {
+                    textBox1.Enabled = true;
+                }));
+                button1.Invoke(new ThreadStart(delegate
+                {
+                    button1.Enabled = true;
+                }));
+                button2.Invoke(new ThreadStart(delegate
+                {
+                    button2.Enabled = true;
+                }));
+                button3.Invoke(new ThreadStart(delegate
+                {
+                    button3.Enabled = true;
+                }));
+                MessageBox.Show("Поиск завершён!");
+            }
+            catch(ThreadAbortException)
+            {
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Ошибка");
+            }
+        }
+
+        private void Compare()
+        {
+            try
+            {
+                List<string> ReadFiles = new List<string>();
+                List<string> ReadAll = new List<string>();
+
+                pictureBox1.Invoke(new ThreadStart(delegate
+                {
+                    pictureBox1.Visible = true;
+                }));
+
+                foreach (string FileName in FileList)
+                {
+                    List<int> ReadStrings = new List<int>();
+
+                    if(!ReadFiles.Contains(FileName))
+                    {
+                        int cnt = 0;
+                        StreamReader sr = new StreamReader(FileName, Encoding.Default);
+                        string line;
+
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            cnt++;
+
+                            bool ToRead = false;
+
+                            string ReplaceLine = Regex.Replace(line, @"[ _,.:;-]+", "");
+
+                            if (!ReadAll.Contains(ReplaceLine))
+                            {
+                                if (!String.IsNullOrEmpty(ReplaceLine))
+                                {
+                                    foreach (string _File in FileList)
+                                    {
+                                        List<int> _ReadStrings = new List<int>();
+
+                                        if (!ReadFiles.Contains(_File))
+                                        {
+                                            if (_File != FileName)
+                                            {
+                                                int _cnt = 0;
+                                                StreamReader _sr = new StreamReader(_File, Encoding.Default);
+                                                string _line;
+                                                while ((_line = _sr.ReadLine()) != null)
+                                                {
+                                                    _cnt++;
+
+                                                    if (!String.IsNullOrEmpty(Regex.Replace(_line, @"[ _,.:;-]+", "")))
+                                                    {
+                                                        if (String.Equals(Regex.Replace(_line, @"[ _,.:;-]+", ""), ReplaceLine, StringComparison.InvariantCultureIgnoreCase))
+                                                        {
+                                                            ToRead = true;
+                                                            if (this.InvokeRequired)
+                                                            {
+                                                                if (!ReadStrings.Contains(cnt))
+                                                                {
+                                                                    listView1.Invoke(new ThreadStart(delegate
+                                                                    {
+                                                                        listView1.Items.Add(new ListViewItem(new string[] { line, FileName, cnt.ToString() }));
+                                                                    }));
+                                                                    ReadStrings.Add(cnt);
+                                                                }
+                                                                if (!_ReadStrings.Contains(_cnt))
+                                                                {
+                                                                    listView1.Invoke(new ThreadStart(delegate
+                                                                    {
+                                                                        listView1.Items.Add(new ListViewItem(new string[] { _line, _File, _cnt.ToString() }));
+                                                                    }));
+                                                                    _ReadStrings.Add(_cnt);
+                                                                }
+                                                            }
+                                                            else
+                                                            {
+                                                                if (!ReadStrings.Contains(cnt) && !_ReadStrings.Contains(_cnt))
+                                                                {
+                                                                    listView1.Invoke(new ThreadStart(delegate
+                                                                    {
+                                                                        listView1.Items.Add(new ListViewItem(new string[] { line, FileName, cnt.ToString() }));
+                                                                    }));
+                                                                    ReadStrings.Add(cnt);
+                                                                    _ReadStrings.Add(_cnt);
+                                                                }
+                                                                listView1.Invoke(new ThreadStart(delegate
+                                                                {
+                                                                    listView1.Items.Add(new ListViewItem(new string[] { _line, _File, _cnt.ToString() }));
+                                                                }));
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                                _sr.Close();
+                                            }
+                                        }
+                                    }
+                                }
+                                if(ToRead)
+                                    ReadAll.Add(Regex.Replace(line, @"\s+", ""));
+                            }
+                        }
+                        sr.Close();
+                    }
+                    if (!ReadFiles.Contains(FileName))
+                        ReadFiles.Add(FileName);
+                }
+                pictureBox1.Invoke(new ThreadStart(delegate
+                {
+                    pictureBox1.Visible = false;
+                }));
+                textBox1.Invoke(new ThreadStart(delegate
+                {
+                    textBox1.Enabled = true;
+                }));
+                button1.Invoke(new ThreadStart(delegate
+                {
+                    button1.Enabled = true;
+                }));
+                button2.Invoke(new ThreadStart(delegate
+                {
+                    button2.Enabled = true;
+                }));
+                button3.Invoke(new ThreadStart(delegate
+                {
+                    button3.Enabled = true;
+                }));
+                MessageBox.Show("Поиск завершён!");
+            }
+            catch (ThreadAbortException)
+            {
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Ошибка");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FileList.Clear();
+            label2.Text = "Всего файлов: 0";
+        }
+    }
+}
